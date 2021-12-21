01e024516227fb5cd19fd413688fecdd4fe274a8
MaxHa-28 <maxhajek@outlook.de>
2021-12-21 12:35:22 +0100

    solution to day_02 a

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+
+struct Position {
+    depth: i32,
+    forward: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("data.txt").expect("Error reading file");
+    let all_commands: Vec<&str> = contents.split("\n").collect();
+
+    let mut position = Position {
+        depth: 0,
+        forward: 0,
+    };
+
+    for command in all_commands.iter() {
+        let command_parts = command.split(" ").collect::<Vec<&str>>();
+        let (text, value): (&str, i32) = (command_parts[0], command_parts[1].parse().unwrap());
+        match text {
+            "forward" => position.forward += value,
+            "up" => position.depth -= value,
+            "down" => position.depth += value,
+            _ => println!("Can't match command: {}", text),
+        }
+    }
+
+    println!("What do you get if you multiply your final horizontal position by your final depth?");
+    println!("Multiplied: {}", position.depth * position.forward)
 }
