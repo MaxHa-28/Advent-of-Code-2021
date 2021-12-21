b9e3b56775c2d601d4cff5f0c515744a5ba58bc6
MaxHa-28 <maxhajek@outlook.de>
2021-12-21 15:41:25 +0100

    day 03 a refactor

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,35 +1,26 @@
 use std::fs;
 
-struct Rates {
-    gamma: Vec<bool>,
-    gamma_decimal: i32,
-    epsilon: Vec<bool>,
-    epsilon_decimal: i32,
-}
-
-fn convert_bool_array_to_decimal(bool_array: &Vec<bool>, num_size: &usize) -> i32 {
-    let mut decimal = 0;
+fn calc_power_consumption(bool_array: &Vec<bool>, num_size: &usize) -> i32 {
+    let mut gamma = 0;
+    let mut epsilon = 0;
     for (idx, val) in bool_array.iter().enumerate() {
         if *val == true {
-            decimal += i32::pow(2, (*num_size - (idx + 1)) as u32)
+            gamma += i32::pow(2, (*num_size - (idx + 1)) as u32)
+        } else {
+            epsilon += i32::pow(2, (*num_size - (idx + 1)) as u32)
         }
     }
-    decimal
+    gamma * epsilon
 }
 
 fn main() {
     let contents = fs::read_to_string("data.txt").expect("Error reading file");
     let report: Vec<&str> = contents.split("\n").collect();
 
-    let mut rates = Rates {
-        gamma: vec![],
-        gamma_decimal: 0,
-        epsilon: vec![],
-        epsilon_decimal: 0,
-    };
-
     let report_length = report.iter().count();
     let num_length = report.first().unwrap().chars().count();
+
+    let mut bool_array = vec![];
 
     for n in 0..num_length {
         let count = report
@@ -41,20 +32,14 @@ fn main() {
             .count();
 
         if count > report_length / 2 {
-            rates.gamma.push(true);
-            rates.epsilon.push(false);
+            bool_array.push(true);
         } else {
-            rates.gamma.push(false);
-            rates.epsilon.push(true);
+            bool_array.push(false);
         }
     }
 
-    rates.gamma_decimal = convert_bool_array_to_decimal(&rates.gamma, &num_length);
-    rates.epsilon_decimal = convert_bool_array_to_decimal(&rates.epsilon, &num_length);
+    let power_consumption = calc_power_consumption(&bool_array, &num_length);
 
     println!("What is the power consumption of the submarine?");
-    println!(
-        "{} units power consumption",
-        rates.epsilon_decimal * rates.gamma_decimal
-    )
+    println!("{} units power consumption", power_consumption)
 }
