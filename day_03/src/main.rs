f0c3bf8fa3f6231c2333c4f5e1ca723f4ee446a3
MaxHa-28 <maxhajek@outlook.de>
2021-12-21 15:30:38 +0100

    day 03 a solution

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -2,7 +2,19 @@ use std::fs;
 
 struct Rates {
     gamma: Vec<bool>,
+    gamma_decimal: i32,
     epsilon: Vec<bool>,
+    epsilon_decimal: i32,
+}
+
+fn convert_bool_array_to_decimal(bool_array: &Vec<bool>, num_size: &usize) -> i32 {
+    let mut decimal = 0;
+    for (idx, val) in bool_array.iter().enumerate() {
+        if *val == true {
+            decimal += i32::pow(2, (*num_size - (idx + 1)) as u32)
+        }
+    }
+    decimal
 }
 
 fn main() {
@@ -11,23 +23,22 @@ fn main() {
 
     let mut rates = Rates {
         gamma: vec![],
+        gamma_decimal: 0,
         epsilon: vec![],
+        epsilon_decimal: 0,
     };
 
     let report_length = report.iter().count();
     let num_length = report.first().unwrap().chars().count();
 
-    let bit_array = report.join("");
-
     for n in 0..num_length {
-        let count = bit_array
+        let count = report
+            .join("")
             .chars()
             .skip(n)
             .step_by(num_length)
             .filter(|e| *e == '1')
             .count();
-
-        println!("{} {}", count, report_length);
 
         if count > report_length / 2 {
             rates.gamma.push(true);
@@ -38,5 +49,12 @@ fn main() {
         }
     }
 
-    println!("{:?}", rates.gamma)
+    rates.gamma_decimal = convert_bool_array_to_decimal(&rates.gamma, &num_length);
+    rates.epsilon_decimal = convert_bool_array_to_decimal(&rates.epsilon, &num_length);
+
+    println!("What is the power consumption of the submarine?");
+    println!(
+        "{} units power consumption",
+        rates.epsilon_decimal * rates.gamma_decimal
+    )
 }
